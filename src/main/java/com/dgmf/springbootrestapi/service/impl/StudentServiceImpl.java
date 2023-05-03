20a23528d528bfa24fc37fbe08e9044097520ac6
FranckDodo91 <devlogappdgmf@gmail.com>
2023-05-03 07:06:41 +0200

    Creation of the StudentServiceImpl class, implementation of the StudentService interface

---
+++ src/main/java/com/dgmf/springbootrestapi/service/impl/StudentServiceImpl.java
@@ -0,0 +1,26 @@
+package com.dgmf.springbootrestapi.service.impl;
+
+import com.dgmf.springbootrestapi.model.Student;
+import com.dgmf.springbootrestapi.service.StudentService;
+
+public class StudentServiceImpl implements StudentService {
+
+	@Override
+	public Student getById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Student getByName(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean save(Student student) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
