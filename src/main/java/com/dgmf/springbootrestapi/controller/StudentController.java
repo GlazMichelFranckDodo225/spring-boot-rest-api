4e71b1191c079812d46b1855c5d50c1740cae8a6
FranckDodo91 <devlogappdgmf@gmail.com>
2023-05-05 09:31:15 +0200

    Setting up the Endpoint @GetMapping('/id'). Endpoint test in the browser ==> Ok

--- src/main/java/com/dgmf/springbootrestapi/controller/StudentController.java
+++ src/main/java/com/dgmf/springbootrestapi/controller/StudentController.java
@@ -1,10 +1,25 @@
 package com.dgmf.springbootrestapi.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.dgmf.springbootrestapi.model.Student;
+import com.dgmf.springbootrestapi.service.StudentService;
 
 @RestController
 @RequestMapping("/student")
 public class StudentController {
+	@Autowired
+	private StudentService studentService;
+	
+	@GetMapping("/id")
+	public Student getById(@RequestParam(value = "id", defaultValue = "1") int id) {
+		return studentService.getById(id);
+	}
+	
+	
 
 }
